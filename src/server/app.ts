bfbd00c21a223b4ad37459d53493fd630a1fc670
Acellam Guy <abiccel@yahoo.com>
2018-10-17 08:17:17 +0300

    Add mongdb connection

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,16 +1,26 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as mongoose from "mongoose";
+
 import { Routes } from "./routes";
 
 class App {
 
     public app: express.Application;
     public routePrv: Routes = new Routes();
+    public mongoUrl = "mongodb://127.0.0.1:27017/metvn";
 
     constructor() {
         this.app = express();
         this.config();
         this.routePrv.routes(this.app);
+
+        this.mongoSetup();
+    }
+
+    private mongoSetup(): void {
+        // mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
     }
 
     private config(): void {
