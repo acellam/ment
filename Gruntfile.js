1b43e4312419158c2bbac39c8358f0d7626b1ccc
Acellam Guy <abiccel@yahoo.com>
2021-02-06 01:08:59 +0300

    Improve maintainability of User Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,43 @@
 const webpackConfig = require("./webpack.config");
 const { merge } = require('webpack-merge');
 
+module.exports = function (grunt) {
+    const pkg = grunt.file.readJSON("package.json");
+    grunt.initConfig({
+        watch: getWatch(),
+        webpack: getWebpack(),
+        clean: getClean(),
+        checkDependencies: {
+            this: {}
+        }
+    });
+
+    getGruntLibs(grunt);
+
+    grunt.registerTask("default", ["clean build", "watch"]);
+    grunt.registerTask(
+        "clean build",
+        "Compiles all the assets and copies the files to the dist directory.",
+        ["checkDependencies", "clean:build", "webpack:develop"]
+    );
+    grunt.registerTask(
+        "release",
+        "Compiles all the assets and copies the files to the dist directory. Minified without source mapping",
+        ["checkDependencies", "clean:build", "webpack:release"]
+    );
+    grunt.registerTask(
+        "test",
+        "Compiles all the assets and copies the files to the dist directory. Minified without source mapping",
+        ["checkDependencies", "clean:build", "webpack:test"]
+    );
+    grunt.registerTask(
+        "developProd",
+        "Compiles all the assets and copies the files to the dist directory. Minified without source mapping",
+        ["checkDependencies", "clean:build", "webpack:developProd"]
+    );
+    grunt.registerTask("build", ["clean build"]);
+};
+
 const webpackConfigTest = merge(webpackConfig, {
     devtool: false,
     mode: "development",
@@ -82,67 +119,42 @@ const webpackConfigRelease = merge(webpackConfig, {
     }
 });
 
-module.exports = function (grunt) {
-    const pkg = grunt.file.readJSON("package.json");
-    grunt.initConfig({
-
-        watch: {
-            updateSourceFiles: {
-                files: ["./src/**/*"],
-                tasks: ["webpack:develop"],
-                options: {
-                    debounceDelay: 250,
-                    livereload: true
-                }
-            }
-        },
-
-        webpack: {
-            develop: webpackConfig,
-            developProd: webpackConfigDevProd,
-            release: webpackConfigRelease,
-            test: webpackConfigTest
-        },
-
-        clean: {
-            build: [
-                "./dist/**/*",
-                "./dist/testresults/**/*",
-            ]
-        },
-
-        checkDependencies: {
-            this: {}
-        }
-    });
-
+function getGruntLibs(grunt) {
     grunt.loadNpmTasks("grunt-check-dependencies");
     grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-file-append");
     grunt.loadNpmTasks("grunt-webpack");
+}
 
-    grunt.registerTask("default", ["clean build", "watch"]);
-    grunt.registerTask(
-        "clean build",
-        "Compiles all the assets and copies the files to the dist directory.",
-        ["checkDependencies", "clean:build", "webpack:develop"]
-    );
-    grunt.registerTask(
-        "release",
-        "Compiles all the assets and copies the files to the dist directory. Minified without source mapping",
-        ["checkDependencies", "clean:build", "webpack:release"]
-    );
-    grunt.registerTask(
-        "test",
-        "Compiles all the assets and copies the files to the dist directory. Minified without source mapping",
-        ["checkDependencies", "clean:build", "webpack:test"]
-    );
-    grunt.registerTask(
-        "developProd",
-        "Compiles all the assets and copies the files to the dist directory. Minified without source mapping",
-        ["checkDependencies", "clean:build", "webpack:developProd"]
-    );
-    grunt.registerTask("build", ["clean build"]);
-};
+function getClean() {
+    return {
+        build: [
+            "./dist/**/*",
+            "./dist/testresults/**/*",
+        ]
+    };
+}
+
+function getWatch() {
+    return {
+        updateSourceFiles: {
+            files: ["./src/**/*"],
+            tasks: ["webpack:develop"],
+            options: {
+                debounceDelay: 250,
+                livereload: true
+            }
+        }
+    };
+}
+
+function getWebpack() {
+    return {
+        develop: webpackConfig,
+        developProd: webpackConfigDevProd,
+        release: webpackConfigRelease,
+        test: webpackConfigTest
+    };
+}
