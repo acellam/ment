3a8d3d0cf6b52acb657eee8b0a82410706445310
Acellam Guy <abiccel@yahoo.com>
2021-02-09 23:35:08 +0300

    Resolve failing tests. #32 should be closed

--- src/__tests__/webauth.spec.ts
+++ src/__tests__/webauth.spec.ts
@@ -16,17 +16,17 @@ describe("# WebAuth", () => {
     it("should not login with the right user but wrong password", () => {
         return request.post(endpoint)
             .send({ username: "testuser", password: "anythingGoesHere" })
-            .expect(401);
+            .expect(400);
     });
 
     it("should return invalid credentials error", () => {
         return request.post(endpoint)
             .send({ username: "testuser", password: "" })
-            .expect(401)
+            .expect(400)
             .then((_res: any) => {
                 return request.post(endpoint)
                     .send({ username: "anotherusername", password: "mypass" })
-                    .expect(401);
+                    .expect(400);
             });
     });
 
