6f4333b872c1757e0a4f280148dae23a63011b3e
Acellam Guy <abiccel@yahoo.com>
2021-02-11 22:23:19 +0300

    Improve maintainability

--- src/controllers/base.ts
+++ src/controllers/base.ts
@@ -5,47 +5,38 @@ export class BaseController {
 
     public createRecord = (model: any, res: Response) => {
         model.save((err: mongoose.CallbackError, dbObject: mongoose.Document) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(dbObject);
+            this.sendResponse(err, res, dbObject);
         });
     }
 
     public getRecords = (model: any, res: Response) => {
         model.find({}, (err: mongoose.CallbackError, dbObject: mongoose.Document) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(dbObject);
+            this.sendResponse(err, res, dbObject);
         });
     }
 
     public getRecordWithID = (model: any, req: Request, res: Response) => {
         model.findById(req.params.contactId, (err: mongoose.CallbackError, dbObject: mongoose.Document) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(dbObject);
+            this.sendResponse(err, res, dbObject);
         });
     }
 
     public updateRecord = (model: any, req: Request, res: Response) => {
         model.findOneAndUpdate({ _id: req.params.contactId }, req.body, { new: true }, (err: mongoose.CallbackError, dbObject: mongoose.Document) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(dbObject);
+            this.sendResponse(err, res, dbObject);
         });
     }
 
     public deleteRecord = (model: any, req: Request, res: Response) => {
-        // tslint: disable-next-line
         model.remove({ _id: req.params.contactId }, (err: mongoose.CallbackError) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json({ message: "Successfully deleted Item!" });
+            this.sendResponse(err, res, { message: "Successfully deleted Item!" });
         });
+    }
+
+    private sendResponse(err: mongoose.CallbackError, res: Response<any, Record<string, any>>, dbObject: {}) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(dbObject);
     }
 }
