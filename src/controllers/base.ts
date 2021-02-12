1732f0f56a5879bfcb467e8f3b247d24b6722d07
Acellam Guy <abiccel@yahoo.com>
2021-02-12 13:56:08 +0300

    Improve base class modifiers

--- src/controllers/base.ts
+++ src/controllers/base.ts
@@ -3,31 +3,31 @@ import * as mongoose from "mongoose";
 
 export class BaseController {
 
-    public createRecord = (model: any, res: Response) => {
+    protected createRecord = (model: any, res: Response) => {
         model.save((err: mongoose.CallbackError, dbObject: mongoose.Document) => {
             this.sendResponse(err, res, dbObject);
         });
     }
 
-    public getRecords = (model: any, res: Response) => {
+    protected getRecords = (model: any, res: Response) => {
         model.find({}, (err: mongoose.CallbackError, dbObject: mongoose.Document) => {
             this.sendResponse(err, res, dbObject);
         });
     }
 
-    public getRecordWithID = (model: any, req: Request, res: Response) => {
+    protected getRecordWithID = (model: any, req: Request, res: Response) => {
         model.findById(req.params.contactId, (err: mongoose.CallbackError, dbObject: mongoose.Document) => {
             this.sendResponse(err, res, dbObject);
         });
     }
 
-    public updateRecord = (model: any, req: Request, res: Response) => {
+    protected updateRecord = (model: any, req: Request, res: Response) => {
         model.findOneAndUpdate({ _id: req.params.contactId }, req.body, { new: true }, (err: mongoose.CallbackError, dbObject: mongoose.Document) => {
             this.sendResponse(err, res, dbObject);
         });
     }
 
-    public deleteRecord = (model: any, req: Request, res: Response) => {
+    protected deleteRecord = (model: any, req: Request, res: Response) => {
         model.remove({ _id: req.params.contactId }, (err: mongoose.CallbackError) => {
             this.sendResponse(err, res, { message: "Successfully deleted Item!" });
         });
