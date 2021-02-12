df12df7eec677b1a24e47d1d569df32bdcb3aa72
Acellam Guy <abiccel@yahoo.com>
2021-02-12 14:00:30 +0300

    Improve on maintainability

--- src/api/contact.ts
+++ src/api/contact.ts
@@ -5,15 +5,16 @@ export class Contact {
     public contactController: ContactController = new ContactController();
 
     public routes = (app: Application) => {
-        app.route(`${process.env.API_BASE}contact`)
+        // All contacts
+        app.route(`${process.env.API_BASE}contacts`)
             // GET endpoint
             .get(this.contactController.getContacts)
             // POST endpoint
             .post(this.contactController.addNewContact);
 
         // Contact detail
-        app.route(`${process.env.API_BASE}contact/:contactId`)
-        // get specific contact
+        app.route(`${process.env.API_BASE}contacts/:contactId`)
+            // get specific contact
             .get(this.contactController.getContactWithID)
             .put(this.contactController.updateContact)
             .delete(this.contactController.deleteContact);
--- src/api/user.ts
+++ src/api/user.ts
@@ -5,12 +5,16 @@ export class User {
     public userController: UserController = new UserController();
 
     public routes = (app: Application) => {
-        app.route(`${process.env.API_BASE}user`)
+        // All Users
+        app.route(`${process.env.API_BASE}users`)
+            // GET endpoint
             .get(this.userController.getUsers)
+            // POST endpoint
             .post(this.userController.addNewUser);
 
         // User detail
-        app.route(`${process.env.API_BASE}user/:userId`)
+        app.route(`${process.env.API_BASE}users/:userId`)
+            // get specific user
             .get(this.userController.getUserWithID)
             .put(this.userController.updateUser)
             .delete(this.userController.deleteUser);
